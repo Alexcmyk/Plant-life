78abe0e71a9d2165651ce4c06581e21e65109aae
Alexcmyk <alex.kirts@gmail.com>
2020-06-14 13:48:23 -0600

    adds if else to menu

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,10 @@ const mainTag = document.querySelector("main");
 
 toggleTag.addEventListener("click", function () {
   mainTag.classList.toggle("open");
+
+  if (mainTag.classList.contains("open")) {
+    toggleTag.innerHTML = `<img src="./img/close.svg"> Close`;
+  } else {
+    toggleTag.innerHTML = `<img src="./img/menu.svg"> Menu`;
+  }
 });
