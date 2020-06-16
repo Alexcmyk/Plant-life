35c6005bb948220a535a27a3fb7dc79490d2d7f8
Alexcmyk <alex.kirts@gmail.com>
2020-06-16 08:28:52 -0600

    added hover effects, drop shadow and transistions

--- js/slides.js
+++ js/slides.js
@@ -4,6 +4,8 @@ const images = slideArea.querySelectorAll("img");
 let currentSlide = 0;
 let z = 1;
 
+// Click slideshow
+
 slideArea.addEventListener("click", function () {
   currentSlide += 1;
 
@@ -12,6 +14,29 @@ slideArea.addEventListener("click", function () {
   }
 
   z += 1;
+
   images[currentSlide].style.zIndex = z;
   images[currentSlide].style.animation = "fade 0.3s";
+});
+
+// Mouse over to random transfom
+
+slideArea.addEventListener("mouseover", function () {
+  images.forEach((image) => {
+    const x = 25 * Math.floor(Math.random() * 5) - 50;
+    const y = 25 * Math.floor(Math.random() * 5) - 50;
+
+    image.style.transform = `translate(${x}px, ${y}px)`;
+  });
+});
+
+// Mouse out to reset
+
+slideArea.addEventListener("mouseout", function () {
+  images.forEach((image) => {
+    const y = 0;
+    const x = 0;
+
+    image.style.transform = "";
+  });
 });
