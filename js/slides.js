7db54a75fbd5b52f500ce018d53a0413ec64ec9a
Alexcmyk <alex.kirts@gmail.com>
2021-05-10 20:55:54 -0600

    fix conflict

--- js/slides.js
+++ js/slides.js
@@ -4,7 +4,6 @@ const images = slideArea.querySelectorAll('img');
 let currentSlide = 0;
 let z = 1;
 
-<<<<<<< HEAD
 // Click slideshow
 
 slideArea.addEventListener('click', function () {
@@ -13,17 +12,9 @@ slideArea.addEventListener('click', function () {
     if (currentSlide > images.length - 1) {
         currentSlide = 0;
     }
-=======
-slideArea.addEventListener("click", () => {
-  currentSlide += 1;
-  if (currentSlide > images.length - 1) {
-    currentSlide = 0;
-  }
->>>>>>> 3687471a267bbd0c6b643e50be0096eb28deebb5
 
     z += 1;
 
-<<<<<<< HEAD
     images[currentSlide].style.zIndex = z;
     images[currentSlide].style.animation = 'fade 0.3s';
 });
@@ -48,27 +39,4 @@ slideArea.addEventListener('mouseout', function () {
 
         image.style.transform = '';
     });
-=======
-  images.forEach((image) => {
-    image.style.animation = "";
-  });
-
-  images[currentSlide].style.zIndex = z;
-  images[currentSlide].style.animation = "fade 0.5s";
-});
-
-slideArea.addEventListener("mouseover", () => {
-  images.forEach((image) => {
-    const x = 25 * Math.floor(Math.random() * 5) - 50;
-    const y = 25 * Math.floor(Math.random() * 5) - 50;
-
-    image.style.transform = `translate(${x}px,${y}px)`;
-  });
-});
-
-slideArea.addEventListener("mouseout", () => {
-  images.forEach((image) => {
-    image.style.transform = "";
-  });
->>>>>>> 3687471a267bbd0c6b643e50be0096eb28deebb5
 });
